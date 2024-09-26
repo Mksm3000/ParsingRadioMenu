6c8c9e9ffc9920efb967c514ac72dac10acce9b9
Maksim Zosimov <mizo-y@yandex.ru>
2024-09-26 15:53:28 +0200

    create readme.md

--- multiliner.py
+++ multiliner.py
@@ -1,11 +1,33 @@
+import re
 
-with open('all.txt', 'r', encoding='utf-8') as file:
+# with open('all.txt', 'r', encoding='utf-8') as file:
+#     content = file.read()
+#
+# lines = content.split('/n')
+#
+# with open('output.txt', 'w', encoding='utf-8') as file:
+#     for line in lines:
+#         file.write(line + '\n')
+
+
+with open('output.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 
-lines = content.split('/n')
+lines = content.split('</a>\n')
 
-with open('output.txt', 'w', encoding='utf-8') as file:
+with open('readme.md', 'a', encoding='utf-8') as file:
+    file.write(
+        '| Флаг         | Название страны | Обозначение '
+        '|\n|--------------|----------------|-------------|\n')
     for line in lines:
-        file.write(line + '\n')
-
-
+        country_pattern = r'alt=[\'"]?(.*?)[\'"]?"'
+        country_match = re.search(country_pattern, line)
+        country = country_match.group(1)
+        flag_pattern = r'src=[\'"]?(.*?)[\'"]?"'
+        flag_match = re.search(flag_pattern, line)
+        flag_url = flag_match.group(1)
+        short_pattern = r'href=[\'"]?(.*?)[\'"]?"'
+        short_match = re.search(short_pattern, line)
+        short_name = short_match.group(1).split('/')[-2]
+        stamp = (f'# | ![{country}]({flag_url}) | {country} | {short_name} |')
+        file.write(stamp + '\n')
