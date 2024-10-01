04b4e1b59f361ae2a7ebf296a15b404641eb269c
Maksim Zosimov <mizo-y@yandex.ru>
2024-10-01 21:38:39 +0200

    исправлен код - замена поиска через requests

--- parsing_radio_menu.py
+++ parsing_radio_menu.py
@@ -1,38 +1,30 @@
-import glob
 import os
+import re
+import shutil
+
 import requests
-from bs4 import BeautifulSoup
-from selene import browser, query, be
-from selenium import webdriver
 
 
 def clean_folder(folder_name):
-    files = glob.glob(os.path.join(folder_name, '*'))
-    for file in files:
-        os.remove(file)
-        print(f'File {file} removed')
+    if os.path.exists(folder_name):
+        shutil.rmtree(folder_name)
+        os.makedirs(folder_name)
+    else:
+        os.makedirs(folder_name)
+
     print('Folder "result" empty')
 
 
-def get_radio(data: list):
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
+def get_link(data: list):
+    call = requests.get(data[1])
+    blocks = call.text.split('\n')
 
-
-    try:
-        temp_url = data[1]
-        browser.open(temp_url)
-        radio_link = browser.element('.stream.display-flex').get(query.attribute(
-            'data-streams'))
-
-        castro = radio_link.split('?n=')[0]
-
-    except:
-        return ''
-    finally:
-        browser.quit()
-    return castro
+    for block in blocks:
+        link_pattern = r'data-streams="(http[s]?://[^"]+)"'
+        link_match = re.search(link_pattern, block)
+        if link_match:
+            url = link_match.group(1)
+            return url
 
 
 def txt_to_m3u8(source_file):
@@ -43,29 +35,24 @@ def txt_to_m3u8(source_file):
 
 
 if __name__ == '__main__':
-
     clean_folder('result')
 
     base_url = 'https://radio.menu'
     country = input('\nEnter short country code: ').lower()
 
     response = requests.get(base_url + f'/stations/facet/country/{country}/')
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    collection = soup.find_all('a',
-                               class_='grid items-center relative w-full bg-white '
-                                      'transition duration-500 shadow ease-in-out '
-                                      'transform hover:-translate-y-1 hover:shadow-lg '
-                                      'select-none cursor-pointer bg-white rounded-md '
-                                      'p-2')
+    items = [x for x in response.text.split('\n') if 'title="Radio station ' in x]
 
     STATIONS = list()
 
-    for item in collection:
-        name = item.attrs['title'].replace('Radio station ', '').replace(',', ' ')
-        link = base_url + item.attrs['href']
-        patch = (name, link)
-        STATIONS.append(patch)
+    for item in items:
+        pattern = r'title="Radio station (.*?)" href="(.*?)"'
+        match = re.search(pattern, item)
+        if match:
+            name = match.group(1)
+            link = base_url + match.group(2)
+            patch = (name, link)
+            STATIONS.append(patch)
 
     print('Список "STATIONS" готов')
 
@@ -80,7 +67,7 @@ if __name__ == '__main__':
 
         with open(file='result/stations.txt', mode='r', encoding='utf-8') as st_file:
             lines = st_file.readlines()
-            print(f'Всего строк: {len(lines)}')
+            print(f'Всего станций: {len(lines)}\n')
 
             for index, line in enumerate(lines):
                 print(f'Добавлена станция №{index + 1} из {len(lines)}')
@@ -89,7 +76,7 @@ if __name__ == '__main__':
                                                          '').replace('(',
                                                                      '').replace(
                     ')', '').split(', ')
-                pure_link = get_radio(station)
+                pure_link = get_link(station)
                 file.write(f'# EXTINF:-1, {station[0]}\n{pure_link}\n')
 
     txt_to_m3u8(f'result/{country} radios.txt')
